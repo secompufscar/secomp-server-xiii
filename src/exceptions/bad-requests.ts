62d7f9b96821120f32b4faedcc9c8c1f5246d703
compermane <comper.mane@gmail.com>
2024-06-07 11:41:42 -0300

    Middleware de autenticação

--- src/exceptions/bad-requests.ts
+++
@@ -1,7 +0,0 @@
-import { ErrorCode, HttpException } from "./root";
-
-export class BadRequestsException extends HttpException{
-    constructor(message: string, errorCode: ErrorCode){
-        super(message, errorCode, 400, message);
-    }
-}
---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from "express";
+import { UnauthorizedUserError } from "../exceptions/exceptions";
+import { User } from "@prisma/client";
+import { prismaClient } from "..";
+import { JWT_SECRET } from "../secrets";
+import * as jwt from "jsonwebtoken"
+import { ApiError } from "../exceptions/api-errors";
+
+type jwtPayload = {
+    id: number
+}
+
+export async function authMiddleware(req: Request, res: Response, next: NextFunction) {
+    try {
+        const { authorization } = req.headers
+        if(!authorization) {
+            throw new UnauthorizedUserError("Não autorizado")
+        }
+        
+        const token = authorization.split(' ')[1]
+        const { id } = jwt.verify(token, JWT_SECRET) as jwtPayload
+        const user = await prismaClient.user.findFirst( { where: { id } } )
+
+        const { senha: _, ...loggedUser } = user as User
+
+        req.user = loggedUser
+        next()
+    }
+    catch(error: any) {
+        if(error instanceof ApiError)
+            res.status(error.statusCode).json( { message: error.message, statusCode: error.statusCode } )
+        else if(error instanceof jwt.TokenExpiredError)
+            res.status(401).json( { message: "Token expirado", statusCode: 401 } )
+        console.error("Erro em acesso: ", error.message)
+    }
+}
